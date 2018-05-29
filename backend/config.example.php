e278c90772882f151bf47de2f1be140cbe24337c
WilliamMooo <961017025@qq.com>
2018-05-29 22:52:57 +0800

    new version

---
+++ backend/config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = 
+[
+    "dsn" => "mysql:dbname=test;host=127.0.0.1",
+    "user" => "root",
+    "password" => "",//mysql password
+    "table" => [
+        "question" => "table_question",
+        "answer" => "table_answer"
+    ],
+    "question_select_num" => 7
+];
